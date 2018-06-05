a755d27aeb013a96052d05a13c12f71bdbb4cd15
Ben Van Meter <vanmeterx@gmail.com>
2018-06-05 16:35:17 -0500

    Fourth Save

--- js/app.js
+++ js/app.js
@@ -2,13 +2,14 @@
 
 
 var travel = prompt('Have I ever traveled to Russia?');
-
 var sanityAnswer = answer.toUpperCase().trim();
+
 console.log('russia? ' + travel);
 
 if (sanityAnswer === 'Y' || sanityAnswer === 'YES') {
   alert('you betcha!');
 }
+
 else if (sanityAnswer === 'N' || sanityAnswer === 'NO') {
   alert('no way hose-ay!');
 }
@@ -16,24 +17,26 @@ else if (sanityAnswer === 'N' || sanityAnswer === 'NO') {
 
 
 var music = prompt('Is SCREAMO the best music genre ever?');
-console.log('screamo ? ' + music);
+var musicAnswer = answer.toUpperCase().trim();
 
-if (sanityAnswer === 'Y' || sanityAnswer === 'YES') {
-  alert('you betcha!');
+console.log('screamo? ' + music);
+
+if (musicAnswer === 'Y' || musicAnswer === 'YES') {
+  alert('a-negative!');
+}
+else if (musicAnswer === 'N' || musicAnswer === 'NO') {
+  alert('correctamundo!');
+}
+
+
+var movieAnswer = prompt('Was Sgt. Pepper\'s the Best Movie Ever?' );
+console.log('best movie ever? ' + movieAnswer);
+
+if (movieAnswer === 'Y' || sanityAnswer === 'YES') {
+  alert('this is a joke right?');
 }
 else if (sanityAnswer === 'N' || sanityAnswer === 'NO') {
-  alert('no way hose-ay!');
-}
-
-
-var answer = prompt('Hmm. Yes or No?');
-console.log('russia? ' + answer);
-
-if (sanityAnswer === 'Y' || sanityAnswer === 'YES') {
-  alert('you betcha!');
-}
-else if (sanityAnswer === 'N' || sanityAnswer === 'NO') {
-  alert('no way hose-ay!');
+  alert('totally agreed!');
 }
 
 
