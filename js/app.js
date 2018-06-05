a768d1ff84bc12ab091313eac8a4b7df71885e43
Ben Van Meter <vanmeterx@gmail.com>
2018-06-05 16:06:23 -0500

    Third Version Save

--- js/app.js
+++ js/app.js
@@ -1,7 +1,59 @@
 'use strict';
-var answer = prompt('Hmm. Yes or No?');
-var sanityAnswer = answer.toUpperCase().trim();
 
-else if (sanityAnswer === 'Y' || sanityAnswer === 'YES') {
-    alert
+
+var travel = prompt('Have I ever traveled to Russia?');
+
+var sanityAnswer = answer.toUpperCase().trim();
+console.log('russia? ' + travel);
+
+if (sanityAnswer === 'Y' || sanityAnswer === 'YES') {
+  alert('you betcha!');
+}
+else if (sanityAnswer === 'N' || sanityAnswer === 'NO') {
+  alert('no way hose-ay!');
+}
+
+
+
+var music = prompt('Is SCREAMO the best music genre ever?');
+console.log('screamo ? ' + music);
+
+if (sanityAnswer === 'Y' || sanityAnswer === 'YES') {
+  alert('you betcha!');
+}
+else if (sanityAnswer === 'N' || sanityAnswer === 'NO') {
+  alert('no way hose-ay!');
+}
+
+
+var answer = prompt('Hmm. Yes or No?');
+console.log('russia? ' + answer);
+
+if (sanityAnswer === 'Y' || sanityAnswer === 'YES') {
+  alert('you betcha!');
+}
+else if (sanityAnswer === 'N' || sanityAnswer === 'NO') {
+  alert('no way hose-ay!');
+}
+
+
+var answer = prompt('Hmm. Yes or No?');
+console.log('russia? ' + answer);
+
+if (sanityAnswer === 'Y' || sanityAnswer === 'YES') {
+  alert('you betcha!');
+}
+else if (sanityAnswer === 'N' || sanityAnswer === 'NO') {
+  alert('no way hose-ay!');
+}
+
+
+var answer = prompt('Hmm. Yes or No?');
+console.log('russia? ' + answer);
+
+if (sanityAnswer === 'Y' || sanityAnswer === 'YES') {
+  alert('you betcha!');
+}
+else if (sanityAnswer === 'N' || sanityAnswer === 'NO') {
+  alert('no way hose-ay!');
 }
