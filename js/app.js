7d119703d15ed8f824e9fc28dab16f4e17c052ca
‘Nathan <ncash13@hotmail.com>
2018-06-07 14:11:32 -0500

    Added functions

--- js/app.js
+++ js/app.js
@@ -1,170 +1,188 @@
 'use strict';
 
 //Tuesday's Assignment:
-var travel = prompt('Have I ever traveled to Russia?');
-var sanityAnswer = travel.toUpperCase().trim();
+var answer = 'Y';
+var wordAnswer = 'YES';
 
-console.log('russia? ' + travel);
+function travel(){
+  var travel = prompt('Have I ever traveled to Russia?');
+  var sanityAnswer = travel.toUpperCase().trim();
 
-if (sanityAnswer === 'Y' || sanityAnswer === 'YES') {
-  alert('You betcha!');
+  console.log('russia? ' + travel);
+
+  if (sanityAnswer === 'Y' || sanityAnswer === 'YES') {
+    alert('You betcha!');
+  }
+
+  else if (sanityAnswer === 'N' || sanityAnswer === 'NO') {
+    alert('No way hose-ay!');
+  }
 }
 
-else if (sanityAnswer === 'N' || sanityAnswer === 'NO') {
-  alert('No way hose-ay!');
-}
-
-
-
-var music = prompt('Is SCREAMO the best music genre ever?');
-var musicAnswer = music.toUpperCase().trim();
+function music(){
+  var music = prompt('Is SCREAMO the best music genre ever?');
+    var musicAnswer = music.toUpperCase().trim();
 
 console.log('Screamo? ' + music);
 
-if (musicAnswer === 'Y' || musicAnswer === 'YES') {
-  alert('Negatory!');
-}
-else if (musicAnswer === 'N' || musicAnswer === 'NO') {
-  alert('Correct-a-mundo!');
-}
-
-
-var movie = prompt('Was Sgt. Pepper\'s the Best Movie Ever?' );
-var movieAnswer = movie.toUpperCase().trim();
-
-console.log('Best movie ever? ' + movie);
-
-if (movieAnswer === 'Y' || movieAnswer === 'YES') {
-  alert('This is a joke right?');
-}
-else if (movieAnswer === 'N' || movieAnswer === 'NO') {
-  alert('Totally agreed!');
+  if (musicAnswer === 'Y' || musicAnswer === 'YES') {
+    alert('Negatory!');
+  }
+  else if (musicAnswer === 'N' || musicAnswer === 'NO') {
+    alert('Correct-a-mundo!');
+  }
 }
 
+function movie(){
+  var movie = prompt('Was Sgt. Pepper\'s the Best Movie Ever?' );
+  var movieAnswer = movie.toUpperCase().trim();
 
-var mac = prompt('Am I a Mac Fanboi?');
-var macAnswer = mac.toUpperCase().trim();
+  console.log('Best movie ever? ' + movie);
 
-console.log('Mac Fanboi? ' + mac);
-
-if (macAnswer === 'Y' || macAnswer === 'YES') {
-  alert('I appreciate Macs, but..');
-}
-else if (macAnswer === 'N' || macAnswer === 'NO') {
-  alert('I wouldn\'t say I\'m totally against Macs..');
-}
-
-
-var spaghet = prompt('Is Spaghetti awesome?');
-var spaghetAnswer = spaghet.toUpperCase().trim();
-
-console.log('Spaghet? ' + spaghet);
-
-if (spaghetAnswer === 'Y' || spaghetAnswer === 'YES') {
-  alert('Heck yeah!');
-}
-else if (spaghetAnswer === 'N' || spaghetAnswer === 'NO') {
-  alert('Incorrect!');
+  if (movieAnswer === 'Y' || movieAnswer === 'YES') {
+    alert('This is a joke right?');
+  }
+  else if (movieAnswer === 'N' || movieAnswer === 'NO') {
+    alert('Totally agreed!');
+  }
 }
 
+function mac(){
+  var mac = prompt('Am I a Mac Fanboi?');
+  var macAnswer = mac.toUpperCase().trim();
+
+  console.log('Mac Fanboi? ' + mac);
+
+  if (macAnswer === 'Y' || macAnswer === 'YES') {
+    alert('I appreciate Macs, but..');
+  }
+  else if (macAnswer === 'N' || macAnswer === 'NO') {
+    alert('I wouldn\'t say I\'m totally against Macs..');
+  }
+}
+
+function spaghet(){
+  var spaghet = prompt('Is Spaghetti awesome?');
+  var spaghetAnswer = spaghet.toUpperCase().trim();
+
+  console.log('Spaghet? ' + spaghet);
+
+  if (spaghetAnswer === 'Y' || spaghetAnswer === 'YES') {
+    alert('Heck yeah!');
+  }
+  else if (spaghetAnswer === 'N' || spaghetAnswer === 'NO') {
+    alert('Incorrect!');
+  }
+}
 
 //Wednesday's assigned project:
 //number guessing.
-var guessNum = prompt('What number am I thinking?');
-console.log('Number Guess: ' + guessNum);
-var guessCount = 1;
+function guessNum(){
+  var guessNum = prompt('What number am I thinking?');
+  console.log('Number Guess: ' + guessNum);
+  var guessCount = 1;
 
 
-//needed help stopping after 7. thanks jess.
-while (guessNum !== 27 & guessCount < 7) {
+  //needed help stopping after 7. thanks jess.
+  while (guessNum !== 27 && guessCount < 7) {
+
+    if (guessNum === null) {
+      console.log('Cancel Button');
+
+      break;
+    }
+
+    var promptWarn = 'Nope!';
+    if (guessNum > 27) {
+      promptWarn = 'Too high!';
+    } else if (guessNum < 27) {
+      promptWarn = 'Too low!';
+    }
+    console.log(promptWarn);
+
+    guessNum = prompt(promptWarn + ' Try again:');
+
+    console.log('Number Guess: ' + guessNum);
+
+    guessCount = guessCount + 1;
+
+    console.log('Guess Count: ' + guessCount);
+  }
+
+  console.log('finished');
 
   if (guessNum === null) {
-    console.log('Cancel Button');
 
-    break;
-  }
-
-  var promptWarn = 'Nope!';
-  if (guessNum > 27) {
-    promptWarn = 'Too high!';
-  } else if (guessNum < 27) {
-    promptWarn = 'Too low!';
-  }
-  console.log(promptWarn);
-
-  guessNum = prompt(promptWarn + ' Try again:');
-
-  console.log('Number Guess: ' + guessNum);
-
-  guessCount = guessCount + 1;
-
-  console.log('Guess Count: ' + guessCount);
-}
-
-console.log('finished');
-
-if (guessNum === null) {
-
-  alert('Cheater. It was 27');
-
-} else {
-  console.log('cancel button');
-
-  if (guessCount === 1) {
-    alert('Woah.. a hole in one!');
-  } else {
-    alert('Sorry! Only 7 guesses! The Correct number was ' + guessNum);
-
-    /* extra stuff i may or may not need.
-  if (guessCount >= 7) {
+    alert('Cheater. It was 27');
 
   } else {
-    console.log('Max 7 Tries');
-    */
+    console.log('cancel button');
 
+    if (guessCount === 1) {
+      alert('Woah.. a hole in one!');
+    } else {
+      alert('Sorry! Only 7 guesses! The Correct number was ' + guessNum);
+
+      /* extra stuff i may or may not need.
+    if (guessCount >= 7) {
+
+    } else {
+      console.log('Max 7 Tries');
+      */
+
+    }
   }
 }
-
 //not sure if this makes sense. attempting to use what i learned above to this set.
 
-var cars = ['MICROBUS', 'CAMARO', 'TESLA', 'CHALLENGER'];
-console.log(cars.length);
-var carGuess = prompt('Guess the dream car!');
-var carGuessTrimmedToUpper = carGuess.toUpperCase().trim();
+function cars(){
+  var cars = ['MICROBUS', 'CAMARO', 'TESLA', 'CHALLENGER'];
+  console.log(cars.length);
+  var carGuess = prompt('Guess the dream car!');
+  var carGuessTrimmedToUpper = carGuess.toUpperCase().trim();
 
-var carCount = 1;
+  var carCount = 1;
 
 
-while (carGuess !== cars & carCount < 7) {
+  while (carGuess !== cars & carCount < 7) {
+
+    if (carGuess === null) {
+      console.log('Cancel Button');
+
+      break;
+    }
+
+    var carWarn = 'Nope!';
+    console.log(carWarn);
+
+    carGuess = prompt(carWarn + ' Try again:');
+
+    console.log('Car Guess: ' + carGuess);
+
+    carCount = carCount + 1;
+
+    console.log('Guess Count: ' + carCount);
+  }
+
+  console.log('finished');
 
   if (carGuess === null) {
-    console.log('Cancel Button');
 
-    break;
-  }
+    alert('Cheater. It was ' + cars.join(', '));
 
-  var carWarn = 'Nope!';
-  console.log(carWarn);
+  } else {
+    console.log('cancel button');
 
-  carGuess = prompt(carWarn + ' Try again:');
-
-  console.log('Car Guess: ' + carGuess);
-
-  carCount = carCount + 1;
-
-  console.log('Guess Count: ' + carCount);
-}
-
-console.log('finished');
-
-if (carGuess === null) {
-
-  alert('Cheater. It was ' + cars.join(', '));
-
-} else {
-  console.log('cancel button');
-
-  if (cars.indexOf(carGuessTrimmedToUpper) >= 0) {
-    alert('Groovy! ' + carGuess + ' is correct!');
+    if (cars.indexOf(carGuessTrimmedToUpper) >= 0) {
+      alert('Groovy! ' + carGuess + ' is correct!');
+    }
   }
 }
+
+travel();
+music();
+movie();
+mac();
+spaghet();
+guessNum();
+cars();
